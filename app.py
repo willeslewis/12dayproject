6aa06ce1b734a0f19294b9bc49d0152467bee453
willeslewis <willeslewis@gmail.com>
2017-12-29 19:40:09 +0000

    changing app.py

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    multiply_text = text * 3
+    return multiply_text
+
 if __name__ == '__main__':
   app.run(port=33507)
