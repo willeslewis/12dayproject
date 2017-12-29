1f34c1a19d3c34eba160fd2f90465d0a2af58970
willeslewis <willeslewis@gmail.com>
2017-12-29 21:05:51 +0000

    plotting

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
-from plots import build_plot
+import numpy as np
+from bokeh.plotting import figure,show,output_notebook
+from bokeh.embed import components 
+import pandas as pd
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def index():
-#  return render_template('index.html')
-
 @app.route('/')
-def render_plot():
-    plot_snippet = build_plot()
-    
-    return render_template('index.html', snippet=plot_snippet)
+def index():
 
-#@app.route('/', methods=['POST'])
-#def my_form_post():
-#    text = request.form['text']
-#    multiply_text = text * 3
-    return multiply_text
+  stock='AAPL'
+  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' % stock
+
+  r=requests.get(api_url)
+  myjson=r.json()
+
+  tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])
+
+  def datetime(x):
+        
+    return np.array(x,dtype=np.datetime64)
+
+  p1 =figure(x_axis_type="datetime",title="Stock Prices")
+
+  p1.grid.grid_line_alpha=0.3
+
+  p1.xaxis.axis_label='Date'
+
+  p1.yaxis.axis_label='Price'
+
+  p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='#A6CEE3',legend='AAPL')
+
+  p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Open'],color='red',legend='AAPL')
+
+  p1.legend.location='top_left'
+
+  script, div = components(p1)
+
+  return render_template('index.html',script=script,div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)
