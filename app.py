60db534032d6cec30a5963aacfcf4a6088694703
willeslewis <willeslewis@gmail.com>
2017-12-29 20:14:53 +0000

    plotting

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+#@app.route('/')
+#def index():
+#  return render_template('index.html')
 
 @app.route('/')
 def render_plot():
     plot_snippet = build_plot()
     
-    return render_template('plots.html', snippet=plot_snippet)
+    return render_template('index.html', snippet=plot_snippet)
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    multiply_text = text * 3
+#@app.route('/', methods=['POST'])
+#def my_form_post():
+#    text = request.form['text']
+#    multiply_text = text * 3
     return multiply_text
 
 if __name__ == '__main__':
