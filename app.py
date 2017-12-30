42bd53ecaab28fc5c0471d9ae3495f0329afe93f
willeslewis <willeslewis@gmail.com>
2017-12-30 05:59:50 +0000

    again

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def plot():
   tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])  
   d = datetime.now() - timedelta(days=365)
 	
-  tickerdata=tickerdata[pd.to_datetime(tickerdata['Date']>=d)]
+  tickerdata=tickerdata[pd.to_datetime(tickerdata['Date'])>=d]
   p1 =figure(x_axis_type="datetime",title="Stock Prices")
 
   p1.xaxis.axis_label='Date'
