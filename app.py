7b07a1c3b4954e634f3efbe015452d3220d1a84c
willeslewis <willeslewis@gmail.com>
2017-12-30 03:31:32 +0000

    again

--- app.py
+++ app.py
@@ -1,106 +1,78 @@
 #import numpy as np
 from bokeh.plotting import figure
 from bokeh.embed import components 
-from bokeh.charts import Histogram
 import pandas as pd
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# Load the Iris Data Set
-iris_df = pd.read_csv("data/iris.data", 
-    names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
+uservars={}
 
-# Create the main plot
-def create_figure(current_feature_name, bins):
-	p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', 
-	 	bins=bins, legend='top_right', width=600, height=400)
-
-	# Set the x axis label
-	p.xaxis.axis_label = current_feature_name
-
-	# Set the y axis label
-	p.yaxis.axis_label = 'Count'
-	return p
+def datetime(x):
+  return np.array(x,dtype=np.datetime64)
 
 
-#def datetime(x):
-#  return np.array(x,dtype=np.datetime64)
+def stock_load(ticker_name):
 
+  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' % stock
+  r=requests.get(api_url)
+  myjson=r.json()
+  tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])
 
-#def stock_load(ticker_name):
+  return tickerdata
 
-#  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' % stock
-#  r=requests.get(api_url)
-#  myjson=r.json()
-#  tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])
-
-#  return tickerdata
-
-#def create_plot(tickerdata,ticker_name):
+def create_plot(tickerdata,ticker_name):
   
-#  p1 =figure(x_axis_type="datetime",title="Stock Prices")
+  p1 =figure(x_axis_type="datetime",title="Stock Prices")
 
-#  p1.grid.grid_line_alpha=0.3
+  p1.grid.grid_line_alpha=0.3
 
-#  p1.xaxis.axis_label='Date'
+  p1.xaxis.axis_label='Date'
 
-#  p1.yaxis.axis_label='Price'
+  p1.yaxis.axis_label='Price'
 
-#  p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='red',legend='AAPL')
+  p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='red',legend='AAPL')
 
-#  p1.legend.location='top_left'
+  p1.legend.location='top_left'
+
+@app.route('/')
+def main():
+  return redirect('/index')
+
 
 # Index page
-@app.route('/')
+@app.route('/index',methods=['GET'])
 def index():
-	# Determine the selected feature
-	current_feature_name = request.args.get("feature_name")
-	if current_feature_name == None:
-		current_feature_name = "Sepal Length"
-
-	# Create the plot
-	plot = create_figure(current_feature_name, 10)
-		
-	# Embed plot into HTML via Flask Render
-	script, div = components(plot)
-	return render_template("iris_index1.html", script=script, div=div,
-		feature_names=feature_names,  current_feature_name=current_feature_name)
+  return render_template('index.html')
 
 
-#@app.route('/')
-#def index():
+@app.rout('/plot.html',method=['POST'])
+def create_plot():
 
-  #stock='AAPL'
-  #api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' % stock
+  uservars['ticker']=request.form['ticker']
 
-  #r=requests.get(api_url)
-  #myjson=r.json()
+  tickerdata=stock_load(request.form['ticker'])
+  
+  p1 =figure(x_axis_type="datetime",title="Stock Prices")
 
-  #tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])
+  p1.grid.grid_line_alpha=0.3
 
-  #def datetime(x):
-        
-  #  return np.array(x,dtype=np.datetime64)
+  p1.xaxis.axis_label='Date'
 
-  #p1 =figure(x_axis_type="datetime",title="Stock Prices")
+  p1.yaxis.axis_label='Price'
 
-  #p1.grid.grid_line_alpha=0.3
+  if request.form.get('Close'):
+    p1.line(datetime(tickerdata['Date']),tickerdata['Close'],color='red',legend='AAPL')
+  if request.form.get('Open'):
+    p1.line(datetime(tickerdata['Date']),tickerdata['Open'],color='red',legend='AAPL')
+  if request.form.get('Adj. Close'):
+    p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='red',legend='AAPL')
+  if request.form.get('Adj. Open'):
+    p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Open'],color='red',legend='AAPL')
+  p1.legend.location='top_left'
 
-  #p1.xaxis.axis_label='Date'
+  script, div = components(p)
 
-  #p1.yaxis.axis_label='Price'
-
-  #p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='#A6CEE3',legend='AAPL')
-
-  #p1.line(datetime(tickerdata['Date']),tickerdata['Adj. Open'],color='red',legend='AAPL')
-
-  #p1.legend.location='top_left'
-
-  #script, div = components(p1)
-
-#  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port=33507)
