0b28e7413947cf573c8fd712c569e0972a574b36
willeslewis <willeslewis@gmail.com>
2017-12-30 04:42:37 +0000

    again

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import numpy as np
 import requests
 from bokeh.plotting import figure
 from bokeh.embed import components 
@@ -33,7 +32,7 @@ def index():
 def plot():
 
   stock=request.form['ticker']
-  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' % stock
+  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.json' %stock
   r=requests.get(api_url)
   myjson=r.json()
   tickerdata=pd.DataFrame(myjson['dataset']['data'],columns=['Date','Open','High','Low','Close','Volume','Ex-Dividend','Split Ratio','Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume'])  
@@ -47,13 +46,13 @@ def plot():
   p1.yaxis.axis_label='Price'
 
   if request.form.get('Close'):
-    p1.line(np.array(tickerdata['Date'],dtype=np.datetime64),tickerdata['Close'],color='red',legend='Close')
+    p1.line(pd.to_datetime(tickerdata['Date']),tickerdata['Close'],color='red',legend='Close')
   if request.form.get('Open'):
-    p1.line(np.array(tickerdata['Date'],dtype=np.datetime64),tickerdata['Open'],color='red',legend='Open')
+    p1.line(pd.to_datetime(tickerdata['Date']),tickerdata['Open'],color='red',legend='Open')
   if request.form.get('Adj. Close'):
-    p1.line(np.array(tickerdata['Date'],dtype=np.datetime64),tickerdata['Adj. Close'],color='red',legend='Adj. Close')
+    p1.line(pd.to_datetime(tickerdata['Date']),tickerdata['Adj. Close'],color='red',legend='Adj. Close')
   if request.form.get('Adj. Open'):
-    p1.line(np.array(tickerdata['Date'],dtype=np.datetime64),tickerdata['Adj. Open'],color='red',legend='Adj. Open')
+    p1.line(pd.to_datetime(tickerdata['Date']),tickerdata['Adj. Open'],color='red',legend='Adj. Open')
   p1.legend.location='top_left'
 
   script, div = components(p1)
